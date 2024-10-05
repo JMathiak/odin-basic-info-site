a55c74b55622255d41231f384ef8e976f5ebb6a1
JMathiak <jmathiak32@yahoo.com>
2024-10-05 11:12:36 -0500

    implemented server

---
+++ index.js
@@ -0,0 +1,33 @@
+let http = require('http')
+let fs = require('fs')
+let url = require ('url')
+
+
+const page404 = fs.readFileSync("404.html", "utf-8", (err, data)=> {
+    if (err) throw err;
+    return data;
+})
+http.createServer(function (req, res){
+    let q = url.parse(req.url, true);
+    let fileName = "";
+    if (q.pathname == "/") {
+      fileName = "." + "/index" + ".html";
+    } else {
+            fileName = "." + q.pathname + ".html";
+        }
+      
+    
+    fs.readFile(fileName, function(err, data){
+        if(err){
+            res.writeHead(404, {'Content-Type': 'text/html'})
+            res.write(page404)
+            return res.end()
+        }else{
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.write(data)
+            return res.end()
+        }
+      
+    })
+   
+}).listen(8080)
