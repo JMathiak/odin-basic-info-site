dc38faf1fe3bef338fcfdef3fb3eee021af76052
JMathiak <jmathiak32@yahoo.com>
2024-10-06 09:36:40 -0500

    installed express + rewrote project using express

--- index.js
+++ index.js
@@ -1,33 +1,55 @@
-let http = require('http')
-let fs = require('fs')
-let url = require ('url')
+// let http = require('http')
+// let fs = require('fs')
+// let url = require ('url')
 
 
-const page404 = fs.readFileSync("404.html", "utf-8", (err, data)=> {
-    if (err) throw err;
-    return data;
-})
-http.createServer(function (req, res){
-    let q = url.parse(req.url, true);
-    let fileName = "";
-    if (q.pathname == "/") {
-      fileName = "." + "/index" + ".html";
-    } else {
-            fileName = "." + q.pathname + ".html";
-        }
+// const page404 = fs.readFileSync("404.html", "utf-8", (err, data)=> {
+//     if (err) throw err;
+//     return data;
+// })
+// http.createServer(function (req, res){
+//     let q = url.parse(req.url, true);
+//     let fileName = "";
+//     if (q.pathname == "/") {
+//       fileName = "." + "/index" + ".html";
+//     } else {
+//             fileName = "." + q.pathname + ".html";
+//         }
       
     
-    fs.readFile(fileName, function(err, data){
-        if(err){
-            res.writeHead(404, {'Content-Type': 'text/html'})
-            res.write(page404)
-            return res.end()
-        }else{
-            res.writeHead(200, {'Content-Type': 'text/html'})
-            res.write(data)
-            return res.end()
-        }
+//     fs.readFile(fileName, function(err, data){
+//         if(err){
+//             res.writeHead(404, {'Content-Type': 'text/html'})
+//             res.write(page404)
+//             return res.end()
+//         }else{
+//             res.writeHead(200, {'Content-Type': 'text/html'})
+//             res.write(data)
+//             return res.end()
+//         }
       
-    })
+//     })
    
-}).listen(8080)
+// }).listen(8080)
+
+
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+app.get('/contact-me', (req, res) => {
+    res.sendFile(path.join(__dirname, '/contact-me.html'))
+})
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/about.html'))
+})
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '/404.html'))
+})
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`My Express app - listening on ${PORT}!`));
